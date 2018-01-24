08fd5b05d3076a8da1651ef0a557dadea581c330
Stacey  Marks <staceyamarks@gmail.com>
2018-01-24 15:55:52 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+# def reverse_each_word(sentence)
+#   sentence_array = sentence.split 
+#   new_array = []
+#   sentence_array.each do |word|
+#     new_array << word.reverse
+#   end
+#   new_array.join(" ")
+# end 
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
